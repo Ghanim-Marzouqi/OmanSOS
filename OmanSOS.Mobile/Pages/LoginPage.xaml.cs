3e4512dade694a72d5c427fbb21e15ff40a0fef4
Ghanim Al Marzouqi <optimist_gm@hotmail.com>
2022-01-02 22:52:10 +0400

    Login Page Design Complete

--- OmanSOS.Mobile/Pages/LoginPage.xaml.cs
+++ OmanSOS.Mobile/Pages/LoginPage.xaml.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 using System;
+using System.Windows.Input;
 
 namespace OmanSOS.Mobile.Pages
 {
     public partial class LoginPage : ContentPage
     {
+        private ICommand GoToRegisterPageCommand => new Command(() => GoToRegisterPage());
+
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void GoToRegisterPage()
+        {
+
         }
     }
 }

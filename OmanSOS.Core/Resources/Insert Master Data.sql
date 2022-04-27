2ee137fc2811b13f5cdade75f74c7569a3255e3b
Ghanim Al Marzouqi <optimist_gm@hotmail.com>
2022-04-27 06:12:30 +0400

    Progress

--- OmanSOS.Core/Resources/Insert Master Data.sql
+++ OmanSOS.Core/Resources/Insert Master Data.sql
@@ -5,6 +5,10 @@ USE OmanSOS;
 INSERT INTO Categories (Id, Name) VALUES (1, 'Food');
 INSERT INTO Categories (Id, Name) VALUES (2, 'Clothes');
 INSERT INTO Categories (Id, Name) VALUES (3, 'Money');
+INSERT INTO Categories (Id, Name) VALUES (4, 'Covid 19');
+INSERT INTO Categories (Id, Name) VALUES (5, 'Zakah');
+INSERT INTO Categories (Id, Name) VALUES (6, 'Fasting Fidyah');
+INSERT INTO Categories (Id, Name) VALUES (7, 'Sadaqa');
 
 INSERT INTO Locations (Id, Name) VALUES (1, 'Muscat');
 INSERT INTO Locations (Id, Name) VALUES (2, 'Bosher');
@@ -15,6 +19,7 @@ INSERT INTO Locations (Id, Name) VALUES (5, 'Salalah');
 INSERT INTO Priorities (Id, Name) VALUES (1, 'Low');
 INSERT INTO Priorities (Id, Name) VALUES (2, 'Medium');
 INSERT INTO Priorities (Id, Name) VALUES (3, 'High');
+INSERT INTO Priorities (Id, Name) VALUES (4, 'Urgent');
 
 INSERT INTO UserTypes (Id, Name) VALUES (1, 'Regular');
 INSERT INTO UserTypes (Id, Name) VALUES (2, 'Administrator');
